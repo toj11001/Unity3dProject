654fc59b611b3a6ab0a9ed970acc39a4b7a87523
Juan Luis de la Cruz <juanlucruz@gmail.com>
2018-01-17 22:06:17 +0100

    Merged with marklessAR project

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    private void Start()
+    {
+        enabled = true;
+    }
+
+    private void Update ()
+    {
+        transform.RotateAround(Camera.main.transform.position, Vector3.up, 10 * Time.deltaTime);
+	}
+}
