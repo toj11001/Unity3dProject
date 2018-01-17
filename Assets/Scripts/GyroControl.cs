2470438caec00a465e28d1b5b807c410c06bc3ba
Juan Luis de la Cruz <juanlucruz@gmail.com>
2018-01-17 22:06:17 +0100

    Merged with marklessAR project

---
+++ Assets/Scripts/GyroControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+    private void Start ()
+    {
+        cameraContainer = new GameObject("Camera Containr");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+	}
+
+    private void Update()
+    {
+        if(gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+        }
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+            return true;
+        }
+        else { return false; }
+    }
+}
---
+++ Assets/Scripts/MarklessAR.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarklessAR : MonoBehaviour
+{
+    // Gyro
+    private Gyroscope gyro;
+    private GameObject cameraContainer;
+    private Quaternion rotation;
+
+    // Cam
+    private WebCamTexture cam;
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private bool arReady = false;
+
+    private void Start()
+    {
+        //Check support for devices
+        //Gyroscope
+        if(!SystemInfo.supportsGyroscope)
+        {
+            Debug.Log("This device does not have Gyroscope");
+            return;
+        }
+
+        // Back Camera
+        for (int i = 0; i < WebCamTexture.devices.Length; i++)
+        {
+            if(!WebCamTexture.devices[i].isFrontFacing)
+            {
+                cam = new WebCamTexture(WebCamTexture.devices[i].name, Screen.width, Screen.height);
+                break;
+            }
+        }
+
+        if (cam == null)
+        {
+            Debug.Log("This device does not have back Camera");
+            return;
+        }
+
+        // Then enable.
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyro = Input.gyro;
+        gyro.enabled = true;
+        cameraContainer.transform.rotation = Quaternion.Euler(90f, 180f, 0);
+        rotation = new Quaternion(0, 0, 1, 0);
+
+        cam.Play();
+        background.texture = cam;
+
+        arReady = true;
+    
+    }
+
+    private void Update()
+    {
+        if (arReady)
+        {
+            // Update Camera
+            float ratio = (float)cam.width / (float)cam.height;
+            fit.aspectRatio = ratio;
+
+            float scaleY = cam.videoVerticallyMirrored ? -1.0f : 1.0f;
+            background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+            int orient = -cam.videoRotationAngle;
+            background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+            // Update Gyro
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+
+}
